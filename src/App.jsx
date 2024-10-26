a2d63b1fc94ab82b651daba9450648a3092a10b8
TheCyberAtom <rkm.rahulkmishra@gmail.com>
2024-10-26 21:46:29 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./components/Home/Home";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app" id="app">
+      <Home />
+    </div>
+  );
+};
+
+export default App;

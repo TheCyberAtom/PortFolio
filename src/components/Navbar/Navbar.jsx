20b84c820255b53229f319ab01f31997cf175968
TheCyberAtom <rkm.rahulkmishra@gmail.com>
2024-11-02 13:31:12 +0530

    Added: Navbar Scroll animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import LaptopMacIcon from "@mui/icons-material/LaptopMac";
 import PermPhoneMsgIcon from "@mui/icons-material/PermPhoneMsg";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import { motion } from "framer-motion";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import "./Navbar.css";
 
-const navbarLoadVariant = {
-  initial: { y: -100, opacity: 0 },
-  animate: { y: 50, opacity: 1 },
-};
-
 const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const { scrollY } = useScroll(0);
+  const lastRef = useRef(0);
+
+  useMotionValueEvent(scrollY, "change", (y) => {
+    const difference = y - lastRef.current;
+    if (Math.abs(difference) > 50) {
+      setIsHidden(difference > 0);
+      lastRef.current = y;
+    }
+  });
+
   return (
     <div className="navbar" id="navbar">
       <motion.div
         className="navbar-content shimmer"
-        variants={navbarLoadVariant}
-        initial="initial"
-        animate="animate"
+        initial={{ y: -150 }}
+        animate={isHidden ? "hidden" : "visible"}
+        variants={{
+          hidden: {
+            y: "-100%",
+          },
+          visible: {
+            y: "125%",
+          },
+        }}
         transition={{
           duration: 1,
           ease: "easeOut",
